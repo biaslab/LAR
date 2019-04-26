0fdb59a7a65755779274458946210a8e82274081
albertpod <albert.podusenko@gmail.com>
2019-04-26 15:34:17 +0200

    Add plots for inference on HAR

--- models/HAR.jl
+++ models/HAR.jl
@@ -1,10 +1,12 @@
 # joint estimations of x and a with known process and measurement noises
-
+## Generate data, build a model and run inference
 using ProgressMeter
 using Revise
 using ForneyLab
 using Random
 using Plots
+using LaTeXStrings
+using StatsPlots
 include( "../AR-node/autoregression.jl")
 include( "../AR-node/observationAR.jl")
 include("../AR-node/rules_prototypes.jl")
@@ -17,28 +19,31 @@ import ForneyLab: unsafeCov, unsafeMean, unsafePrecision
 
 Random.seed!(42)
 
-ARorder = 2
+ARorder = 1
 
-data = generateHAR(1000, ARorder, levels=2, nvars=[0.1, 0.5])
-x = [x[1] for x in data[3]]
+v_θ1 = 0.1
+v_x = 0.5
+
+dataHAR = generateHAR(1000, ARorder, levels=2, nvars=[v_θ1, v_x])
+x = [x[1] for x in dataHAR[3]]
 # Observations
 v_y = 1.0
-y = [xi[1] + sqrt(v_y)*randn() for xi in data[3]]
+y = [xi[1] + sqrt(v_y)*randn() for xi in dataHAR[3]]
 
 # Creating the graph
 g = FactorGraph()
 
-# First layer
-@RV m_θ1
-@RV w_θ1
-@RV θ1 ~ GaussianMeanPrecision(m_θ1, w_θ1)
-@RV a_θ2
-@RV b_θ2
-@RV w_θ2 ~ Gamma(a_θ2, b_θ2)
-@RV m_θ2_t_prev
-@RV w_θ2_t_prev
-@RV θ2_t_prev ~ GaussianMeanPrecision(m_θ2_t_prev, w_θ2_t_prev)
-@RV θ2_t = AR(θ1, θ2_t_prev, w_θ2)
+# Upper layer
+@RV m_θ2
+@RV w_θ2
+@RV θ2 ~ GaussianMeanPrecision(m_θ2, w_θ2)
+@RV a_θ1
+@RV b_θ1
+@RV w_θ1 ~ Gamma(a_θ1, b_θ1)
+@RV m_θ1_t_prev
+@RV w_θ1_t_prev
+@RV θ1_t_prev ~ GaussianMeanPrecision(m_θ1_t_prev, w_θ1_t_prev)
+@RV θ1_t = AR(θ2, θ1_t_prev, w_θ1)
 
 # Second layer
 @RV a_x
@@ -47,7 +52,7 @@ g = FactorGraph()
 @RV m_x_t_prev
 @RV w_x_t_prev
 @RV x_t_prev ~ GaussianMeanPrecision(m_x_t_prev, w_x_t_prev)
-@RV x_t = AR(θ2_t, x_t_prev, w_x)
+@RV x_t = AR(θ1_t, x_t_prev, w_x)
 
 # Observation
 @RV m_y_t
@@ -55,12 +60,12 @@ g = FactorGraph()
 observationAR(m_y_t, x_t, w_y_t)
 
 # Placeholders for prior of upper layer
-placeholder(m_θ1, :m_θ1, dims=(ARorder,))
-placeholder(w_θ1, :w_θ1, dims=(ARorder, ARorder))
-placeholder(a_θ2, :a_θ2)
-placeholder(b_θ2, :b_θ2)
-placeholder(m_θ2_t_prev, :m_θ2_t_prev, dims=(ARorder,))
-placeholder(w_θ2_t_prev, :w_θ2_t_prev, dims=(ARorder, ARorder))
+placeholder(m_θ2, :m_θ2, dims=(ARorder,))
+placeholder(w_θ2, :w_θ2, dims=(ARorder, ARorder))
+placeholder(a_θ1, :a_θ1)
+placeholder(b_θ1, :b_θ1)
+placeholder(m_θ1_t_prev, :m_θ1_t_prev, dims=(ARorder,))
+placeholder(w_θ1_t_prev, :w_θ1_t_prev, dims=(ARorder, ARorder))
 
 # Placeholders for prior of bottom layer
 placeholder(a_x, :a_x)
@@ -75,8 +80,8 @@ placeholder(w_y_t, :w_y_t)
 ForneyLab.draw(g)
 
 # Specify recognition factorization
-q = RecognitionFactorization(θ2_t, θ1, θ2_t_prev, w_θ2, x_t, x_t_prev, w_x,
-                            ids=[:θ2_t :θ1 :θ2_t_prev :W_θ2 :X_t :X_t_prev :W_x])
+q = RecognitionFactorization(θ1_t, θ2, θ1_t_prev, w_θ1, x_t, x_t_prev, w_x,
+                            ids=[:θ1_t :θ2 :θ1_t_prev :W_θ1 :X_t :X_t_prev :W_x])
 
 algo = variationalAlgorithm(q)
 algoF = freeEnergyAlgorithm(q)
@@ -87,12 +92,14 @@ eval(Meta.parse(algoF))
 
 
 # Storage for upper layer
-m_θ2 = Vector{Vector{Float64}}(undef, length(y))
-w_θ2 = Vector{Array{Float64, 2}}(undef, length(y))
 m_θ1 = Vector{Vector{Float64}}(undef, length(y))
 w_θ1 = Vector{Array{Float64, 2}}(undef, length(y))
-a_θ2 = Vector{Float64}(undef, length(y))
-b_θ2 = Vector{Float64}(undef, length(y))
+m_θ1_prev = Vector{Vector{Float64}}(undef, length(y))
+w_θ1_prev = Vector{Array{Float64, 2}}(undef, length(y))
+m_θ2 = Vector{Vector{Float64}}(undef, length(y))
+w_θ2 = Vector{Array{Float64, 2}}(undef, length(y))
+a_θ1 = Vector{Float64}(undef, length(y))
+b_θ1 = Vector{Float64}(undef, length(y))
 
 # Storage for bottom layer
 m_x = Vector{Vector{Float64}}(undef, length(y))
@@ -101,34 +108,34 @@ a_x = Vector{Float64}(undef, length(y))
 b_x = Vector{Float64}(undef, length(y))
 
 # Define values for upper layer
-m_θ1_0 = zeros(ARorder)
-w_θ1_0 = diagAR(ARorder)
-a_θ2_0 = 1
-b_θ2_0 = 0.1
-m_θ2_prev_0 = zeros(ARorder)
-w_θ2_prev_0 = diagAR(ARorder)
-m_θ2_t_0 = zeros(ARorder)
-w_θ2_t_0 = diagAR(ARorder)
+m_θ2_0 = zeros(ARorder)
+w_θ2_0 = diagAR(ARorder)
+a_θ1_0 = 0.0001
+b_θ1_0 = 0.0001
+m_θ1_prev_0 = zeros(ARorder)
+w_θ1_prev_0 = 0.1*diagAR(ARorder)
+m_θ1_t_0 = zeros(ARorder)
+w_θ1_t_0 = 0.1*diagAR(ARorder)
 
 # Define values for bottom layer
-a_x_0 = 1
-b_x_0 = 0.5
+a_x_0 = 0.0001
+b_x_0 = 0.0001
 m_x_t_prev_0 = zeros(ARorder)
 w_x_t_prev_0 = diagAR(ARorder)
 
 # Priors upper layer
-m_θ1_min = m_θ1_0
-w_θ1_min = w_θ1_0
-a_θ2_min = a_θ2_0
-b_θ2_min = b_θ2_0
-m_θ2_t_prev_min = m_θ2_prev_0
-w_θ2_t_prev_min = w_θ2_prev_0
+m_θ2_min = m_θ2_0
+w_θ2_min = w_θ2_0
+a_θ1_min = a_θ1_0
+b_θ1_min = b_θ1_0
+m_θ1_t_prev_min = m_θ1_prev_0
+w_θ1_t_prev_min = w_θ1_prev_0
 
 # Priors bottom layer
 a_x_min = a_x_0
 b_x_min = b_x_0
-m_θ2_t_min = m_θ2_t_0
-w_θ2_t_min = w_θ2_t_0
+m_θ1_t_min = m_θ1_t_0
+w_θ1_t_min = w_θ1_t_0
 m_x_t_prev_min = m_x_t_prev_0
 w_x_t_prev_min = w_x_t_prev_0
 
@@ -144,69 +151,124 @@ p = Progress(length(y), 1, "Observed ")
 for t in 1:length(y)
     update!(p, t)
     # Upper Layer
-    marginals[:θ1] = ProbabilityDistribution(Multivariate, GaussianMeanPrecision, m=m_θ1_min, w=w_θ1_min)
-    marginals[:θ2_t_prev] = ProbabilityDistribution(Multivariate, GaussianMeanPrecision, m=m_θ2_t_prev_min, w=w_θ2_t_prev_min)
-    marginals[:θ2_t] = ProbabilityDistribution(Multivariate, GaussianMeanPrecision, m=m_θ2_t_min, w=w_θ2_t_min)
-    marginals[:w_θ2] = ProbabilityDistribution(Univariate, Gamma, a=a_θ2_min, b=b_θ2_min)
+    marginals[:θ2] = ProbabilityDistribution(Multivariate, GaussianMeanPrecision, m=m_θ2_min, w=w_θ2_min)
+    marginals[:θ1_t_prev] = ProbabilityDistribution(Multivariate, GaussianMeanPrecision, m=m_θ1_t_prev_min, w=w_θ1_t_prev_min)
+    marginals[:θ1_t] = ProbabilityDistribution(Multivariate, GaussianMeanPrecision, m=m_θ1_t_min, w=w_θ1_t_min)
+    marginals[:w_θ1] = ProbabilityDistribution(Univariate, Gamma, a=a_θ1_min, b=b_θ1_min)
     # Bottom Layer
     marginals[:x_t_prev] = ProbabilityDistribution(Multivariate, GaussianMeanPrecision, m=m_x_t_prev_min, w=w_x_t_prev_min)
-    marginals[:w_x] = ProbabilityDistribution(Univariate, Gamma, a=a_θ2_min, b=b_θ2_min)
+    marginals[:w_x] = ProbabilityDistribution(Univariate, Gamma, a=a_x_min, b=b_x_min)
 
-    global m_θ1_min, w_θ1_min, m_θ2_t_prev_min, w_θ2_t_prev_min, a_θ2_min, b_θ2_min,
-           m_x_t_prev_min, w_x_t_prev_min, a_x_min, b_x_min, data
+    global m_θ2_min, w_θ2_min, m_θ1_t_prev_min, w_θ1_t_prev_min, a_θ1_min, b_θ1_min,
+           m_θ1_t_min, w_θ1_t_min, m_x_t_prev_min, w_x_t_prev_min, a_x_min, b_x_min, data
 
     for i = 1:n_its
         data = Dict(:m_y_t => y[t],
                     :w_y_t => v_y^-1,
-                    :m_θ1 => m_θ1_min,
-                    :w_θ1 => w_θ1_min,
-                    :m_θ2_t_prev => m_θ2_t_prev_min,
-                    :w_θ2_t_prev => w_θ2_t_prev_min,
-                    :a_θ2 => a_θ2_min,
-                    :b_θ2 => b_θ2_min,
+                    :m_θ2 => m_θ2_min,
+                    :w_θ2 => w_θ2_min,
+                    :m_θ1_t_prev => m_θ1_t_prev_min,
+                    :w_θ1_t_prev => w_θ1_t_prev_min,
+                    :a_θ1 => a_θ1_min,
+                    :b_θ1 => b_θ1_min,
                     :m_x_t_prev => m_x_t_prev_min,
                     :w_x_t_prev => w_x_t_prev_min,
                     :a_x => a_x_min,
                     :b_x => b_x_min)
 
         stepX_t!(data, marginals)
-        stepW_x!(data, marginals)
-        stepθ2_t!(data, marginals)
-        stepθ1!(data, marginals)
-        stepW_θ2!(data, marginals)
-
-        stepθ2_t_prev!(data, marginals)
-        stepθ2_t!(data, marginals)
-
         stepX_t_prev!(data, marginals)
-        stepX_t!(data, marginals)
+        stepW_x!(data, marginals)
+        stepθ1_t!(data, marginals)
+        stepθ1_t_prev!(data, marginals)
+        stepW_θ1!(data, marginals)
+        stepθ2!(data, marginals)
 
-        m_θ2[t] = unsafeMean(marginals[:θ2_t])
-        w_θ2[t] = unsafePrecision(marginals[:θ2_t])
-        m_θ1[t] = unsafeMean(marginals[:θ1])
-        w_θ1[t] = unsafePrecision(marginals[:θ1])
-        a_θ2[t] = marginals[:w_θ2].params[:a]
-        b_θ2[t] = marginals[:w_θ2].params[:b]
+        m_θ1_prev[t] = unsafeMean(marginals[:θ1_t_prev])
+        w_θ1_prev[t] = unsafePrecision(marginals[:θ1_t_prev])
+        m_θ1[t] = unsafeMean(marginals[:θ1_t])
+        w_θ1[t] = unsafePrecision(marginals[:θ1_t])
+        m_θ2[t] = unsafeMean(marginals[:θ2])
+        w_θ2[t] = unsafePrecision(marginals[:θ2])
+        a_θ1[t] = marginals[:w_θ1].params[:a]
+        b_θ1[t] = marginals[:w_θ1].params[:b]
         m_x[t] = unsafeMean(marginals[:x_t])
         w_x[t] = unsafePrecision(marginals[:x_t])
         a_x[t] = marginals[:w_x].params[:a]
         b_x[t] = marginals[:w_x].params[:b]
 
-        m_θ1_min = m_θ1[t]
-        w_θ1_min = w_θ1[t]
-        m_θ2_t_prev_min = m_θ2[t]
-        w_θ2_t_prev_min = w_θ2[t]
+        m_θ2_min = m_θ2[t]
+        w_θ2_min = w_θ2[t]
+        m_θ1_t_prev_min = m_θ1_prev[t]
+        w_θ1_t_prev_min = w_θ1_prev[t]
+        m_θ1_t_min = m_θ1[t]
+        m_θ1_t_min = m_θ1[t]
         m_x_t_prev_min = m_x[t]
         w_x_t_prev_min = w_x[t]
-        a_θ2_min = a_θ2[t]
-        b_θ2_min = b_θ2[t]
+        a_θ1_min = a_θ1[t]
+        b_θ1_min = b_θ1[t]
         a_x_min = a_x[t]
         b_x_min = b_x[t]
     end
     push!(FAR, (freeEnergy(data, marginals)))
 end
+## Plotting AR coefs first layer
+# Extracting estimates for x_t (first component of vector x̄_t)
+m_xt = [m_x[1] for m_x in m_x]
+v_xt = [v_x[1]^-1 for v_x in w_x]
 
-from = 500; upto=550
-scatter(y[from:upto], label="observed")
-plot!(x[from:upto], label="real")
-plot!([m_x[1] for m_x in m_x[from:upto]], label="inferred")
+# Extracting estimates for θ2_t (first component of vector θ̂2_t)
+m_θ1t = [m_θ1[1] for m_θ1 in m_θ1]
+v_θ1t = [v_x[1]^-1 for v_x in w_θ1]
+
+from = 1; upto = 50;
+scatter(y, markershape = :xcross, markeralpha = 0.6,
+        markersize = 2, xlabel="time t", ylabel="value", label="observations", xlims=(from, upto))
+plot!(x, color=:magenta, label=L"real \quad x_t", title="AR($ARorder) process")
+plot!(m_xt, ribbon=(sqrt.(v_xt), sqrt.(v_xt)),
+      linestyle=:dash, linewidth = 2,
+      color=:black,
+      fillalpha = 0.2,
+      fillcolor = :black,
+      label="inferred")
+
+## Plotting AR coefs second layer
+θ1 = [θ[1] for θ in dataHAR[2]]
+plot(xlims=(from, upto))
+plot!(θ1, color=:red, label=L"true \quad \theta^{(1)}", title="Generated AR($ARorder) process")
+plot!(m_θ1t, ribbon=(sqrt.(v_θ1t), sqrt.(v_θ1t)),
+      linestyle=:dash, linewidth = 2,
+      color=:black,
+      fillalpha = 0.2,
+      fillcolor = :black,
+      label="inferred")
+
+## Plot estimates for variance of first layer
+using Distributions
+bound = 10
+scatter([v_x], [0], xlims=(0, bound), markershape = :diamond,
+       markersize = 5, markeralpha = 0.6, markercolor = :blue, label="true", title=L"\gamma_{x}")
+plot!(Distributions.Gamma(marginals[:w_x].params[:a], 1/marginals[:w_x].params[:b]), lw=1, label="posterior")
+plot!(Distributions.Gamma(a_x_0, 1/b_x_0), lw=1, label="prior")
+
+## Plot estimates for variance of second layer
+bound = 5
+scatter([v_θ1], [0], xlims=(0, bound), markershape = :diamond,
+         markersize = 5, markeralpha = 0.6, markercolor = :blue, label="true", title=L"\gamma_{\theta^{(1)}}")
+plot!(Distributions.Gamma(marginals[:w_θ1].params[:a], 1/marginals[:w_θ1].params[:b]), lw=1, label="posterior")
+plot!(Distributions.Gamma(a_θ1_0, 1/b_θ1_0), lw=1, label="prior")
+
+println("=========\nFirst layer noise variance:")
+println("Estimated ", mean(marginals[:w_x])^-1)
+println("True ", v_x)
+
+println("=========\nSecond layer noise variance:")
+println("Estimated ", mean(marginals[:w_θ1])^-1)
+println("True ", v_θ1)
+
+## Plotting inferred coefs (1D)
+bound = 1
+scatter([dataHAR[1]], [0], xlims=(dataHAR[1][1] - bound, dataHAR[1][1] + bound), markershape = :diamond,
+         markersize = 5, markeralpha = 0.6, markercolor = :blue, label="true", title=L"\theta^{(2)}")
+plot!(Distributions.Normal(mean(marginals[:θ2])[1], cov(marginals[:θ2])[1]), lw=1, label="posterior")
+plot!(Distributions.Normal(m_θ2_0[1], w_θ2_0[1]^-1), lw=1, label="prior")
