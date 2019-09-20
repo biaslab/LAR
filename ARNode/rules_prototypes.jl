385f0d4d5b8a973043b612bd7a573f6d3bfe7f71
albertpod <albert.podusenko@gmail.com>
2019-09-20 16:16:49 +0200

    Add structured VMP rules prototypes

--- ARNode/rules_prototypes.jl
+++ ARNode/rules_prototypes.jl
@@ -18,3 +18,23 @@
                       :outbound_type => Message{Gamma},
                       :inbound_types => (ProbabilityDistribution, ProbabilityDistribution, ProbabilityDistribution, Nothing),
                       :name          => VariationalARIn3PPPN)
+
+@structuredVariationalRule(:node_type     => Autoregression,
+                           :outbound_type => Message{GaussianMeanVariance},
+                           :inbound_types => (Nothing, ProbabilityDistribution, ProbabilityDistribution, ProbabilityDistribution),
+                           :name          => SVariationalAROutNPPP)
+
+@structuredVariationalRule(:node_type     => Autoregression,
+                           :outbound_type => Message{GaussianMeanVariance},
+                           :inbound_types => (ProbabilityDistribution, Nothing, ProbabilityDistribution, ProbabilityDistribution),
+                           :name          => SVariationalARIn1PNPP)
+
+@structuredVariationalRule(:node_type     => Autoregression,
+                           :outbound_type => Message{GaussianMeanVariance},
+                           :inbound_types => (ProbabilityDistribution, ProbabilityDistribution, Nothing, ProbabilityDistribution),
+                           :name          => SVariationalARIn2PPNP)
+
+@structuredVariationalRule(:node_type     => Autoregression,
+                           :outbound_type => Message{Gamma},
+                           :inbound_types => (ProbabilityDistribution, ProbabilityDistribution, ProbabilityDistribution, Nothing),
+                           :name          => SVariationalARIn3PPPN)
--- ARNode/vmp_rules.jl
+++ ARNode/vmp_rules.jl
@@ -19,6 +19,12 @@ function wMatrix(γ, order)
     return mW
 end
 
+function transition(γ, order)
+    V = zeros(order, order)
+    V[1] = 1/γ
+    return V
+end
+
 function shift(dim)
     S = diagAR(dim)
     for i in dim:-1:2
@@ -41,6 +47,14 @@ function defineOrder(dim)
     S = shift(order)
 end
 
+function shiftD(matrix, γ)
+    for i in 2:size(matrix)[1]
+        matrix[i, i] = matrix[i - 1, i - 1]
+    end
+    matrix[1] = γ
+    return matrix
+end
+
 function ruleVariationalAROutNPPP(marg_y :: Nothing,
                                   marg_x :: ProbabilityDistribution{Multivariate},
                                   marg_θ :: ProbabilityDistribution{Multivariate},
@@ -50,7 +64,8 @@ function ruleVariationalAROutNPPP(marg_y :: Nothing,
                        defineOrder(length(mθ)) : order
     mA = S+c*mθ'
     m = mA*unsafeMean(marg_x)
-    W = Hermitian(wMatrix(unsafeMean(marg_γ), order))
+    #W = Hermitian(mθ'*wMatrix(unsafeMean(marg_γ)*mθ, order))
+    W = Hermitian(shiftD(unsafeCov(marg_x), mθ'*unsafeMean(marg_γ)*mθ))
     Message(Multivariate, GaussianWeightedMeanPrecision, xi=W*m, w=W)
 end
 
@@ -63,8 +78,8 @@ function ruleVariationalARIn1PNPP(marg_y :: ProbabilityDistribution{Multivariate
                        defineOrder(length(mθ)) : order
     mA = S+c*mθ'
     mγ = unsafeMean(marg_γ)
-    W = Hermitian(unsafeCov(marg_θ)*mγ+mA'*wMatrix(mγ, order)*mA)
-    xi = mA'*wMatrix(mγ, order)*unsafeMean(marg_y)
+    W = Hermitian(mγ*(unsafeCov(marg_θ) + mθ*mθ'))#Hermitian(unsafeCov(marg_θ)*mγ+mA'*wMatrix(mγ, order)*mA)
+    xi = mθ*unsafeMean(marg_y)[1]#mA'*wMatrix(mγ, order)*unsafeMean(marg_y)
     Message(Multivariate, GaussianWeightedMeanPrecision, xi=xi, w=W)
 end
 
@@ -91,4 +106,76 @@ function ruleVariationalARIn3PPPN(marg_y :: ProbabilityDistribution{Multivariate
     mx = unsafeMean(marg_x)
     B = unsafeCov(marg_y)[1, 1] + my[1]*my[1] - 2*my[1]*mθ'*mx + mθ'*(unsafeCov(marg_x)+mx*mx')*mθ + tr(unsafeCov(marg_θ)*unsafeCov(marg_x)) + mx'*unsafeCov(marg_θ)*mx
     Message(Gamma, a=3/2, b=B/2)
+end
+
+# Structured updated
+
+function ruleSVariationalAROutNPPP(marg_y :: Nothing,
+                                   marg_x :: ProbabilityDistribution{Multivariate},
+                                   marg_θ :: ProbabilityDistribution{Multivariate},
+                                   marg_γ :: ProbabilityDistribution{Univariate})
+    mθ = unsafeMean(marg_θ)
+    order == Nothing ? defineOrder(length(mθ)) : order != length(mθ) ?
+                       defineOrder(length(mθ)) : order
+    mA = S+c*mθ'
+    mx = unsafeMean(marg_x)
+    mγ = unsafeMean(marg_γ)
+    Vx = unsafeCov(marg_x)
+    Vθ = unsafeCov(marg_θ)
+    D = inv(Vx) + mγ*Vθ)
+    my = mA*inv(D)*inv(Vx)*mx
+    trans = transition(mγ, order)
+    Vy = mA*inv(D)*mA' + trans
+    Message(Multivariate, GaussianMeanVariance, m=my, v=Vy)
+end
+
+function ruleSVariationalARIn1PNPP(marg_y :: ProbabilityDistribution{Multivariate},
+                                   marg_x :: Nothing,
+                                   marg_θ :: ProbabilityDistribution{Multivariate},
+                                   marg_γ :: ProbabilityDistribution{Univariate})
+    mθ = unsafeMean(marg_θ)
+    order == Nothing ? defineOrder(length(mθ)) : order != length(mθ) ?
+                       defineOrder(length(mθ)) : order
+    my = unsafeMean(marg_y)
+    mγ = unsafeMean(marg_γ)
+    Vy = unsafeCov(marg_y)
+    Vθ = unsafeCov(marg_θ)
+    trans = transition(mγ, order)
+    G = inv(mA) + inv(mA)*trans*inv(mA)'*inv(inv(mγ)*Vθ + inv(mA)*trans*inv(mA)')*inv(mA)
+    mx = G*(my-Vy*inv(trans + Vy)*my)
+    Vx = G*(Vy - Vy*inv(trans+Vy)*Vy)*G' + Vy - inv(trans + Vy)*Vy
+    Message(Multivariate, GaussianMeanVariance, x=mx, v=Vx)
+end
+
+function ruleSVariationalARIn2PPNP(marg_y :: ProbabilityDistribution{Multivariate},
+                                   marg_x :: ProbabilityDistribution{Multivariate},
+                                   marg_θ :: Nothing,
+                                   marg_γ :: ProbabilityDistribution{Univariate})
+    mγ = unsafeMean(marg_γ)
+    m̂y = unsafeMean(marg_y)
+    m̂x = unsafeMean(marg_x)
+    V̂y = unsafeCov(marg_y)
+    V̂x = unsafeCov(marg_x)
+    order == Nothing ? defineOrder(length(my)) : order != length(my) ?
+                       defineOrder(length(my)) : order
+    mx = unsafeMean(marg_x)
+    mγ = unsafeMean(marg_γ)
+    W = Hermitian(unsafeCov(marg_x)*mγ+mx*mγ*mx')
+    xi = (mx*c'*wMatrix(mγ, order)*my)
+    Message(Multivariate, GaussianWeightedMeanPrecision, xi=xi, w=W)
+end
+
+function ruleSVariationalARIn3PPPN(marg_y :: ProbabilityDistribution{Multivariate},
+                                   marg_x :: ProbabilityDistribution{Multivariate},
+                                   marg_θ :: ProbabilityDistribution{Multivariate},
+                                   marg_γ :: Nothing)
+    mθ = unsafeMean(marg_θ)
+    mA = S+c*mθ'
+    my = unsafeMean(marg_y)
+    mx = unsafeMean(marg_x)
+    Vy = unsafeCov(marg_y)
+    Vx = unsafeCov(marg_x)
+    Vθ = unsafeCov(marg_θ)
+    B = (mA*(Vx + mx*mx)'*mA')[1, 1] + tr(Vθ*Vx) + mx'*Vθ*mx
+    Message(Gamma, a=3/2, b=B)
 end
