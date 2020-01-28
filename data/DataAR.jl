3feee39b867e96a10dea2a631e696e63676e794c
albertpod <albert.podusenko@gmail.com>
2020-01-28 14:38:14 +0100

    Add time-varying variance generator (HGF)

--- data/DataAR.jl
+++ data/DataAR.jl
@@ -5,7 +5,7 @@ using DataFrames
 import PolynomialRoots.roots
 using Distributions
 
-export loadAR, generateAR, generateHAR, generateSIN, writeAR, readAR
+export loadAR, generateAR, generateHAR, generateSIN, writeAR, readAR, generateHGFAR
 
 # Filter bad data
 FloatParse(x) =
@@ -67,6 +67,30 @@ function generateSWAR(num::Int, order::Int; nvar=1, pi=0.5)
     return coefs1, coefs2, _pis, data
 end
 
+function time_varying_variance(kappa, omega, vtop,n_samples)
+    x1, x2 = Array{Float64}(undef,n_samples+1), Array{Float64}(undef,n_samples)
+    x1[1] = randn()
+    for i=1:n_samples
+        x1[i+1] = x1[i] + sqrt(vtop)*randn()
+        x2[i] = exp(kappa*x1[i+1] +omega)
+    end
+    return x1, x2
+end
+
+
+function generateHGFAR(num::Int, order::Int, kappa, omega, vtop)
+    coefs = generate_coefficients(order)
+    inits = randn(order)
+    data = Vector{Vector{Float64}}(undef, num+3*order)
+    data[1] = inits
+    rw, tvar = time_varying_variance(kappa, omega, vtop, num+3*order)
+    for i in 2:num+3*order
+        data[i] = insert!(data[i-1][1:end-1], 1, rand(Distributions.Normal(coefs'data[i-1], sqrt(tvar[i])), 1)[1])
+    end
+    data = data[1+3*order:end]
+    return coefs, data, tvar[1+3*order:end], rw[1+3*order:end]
+end
+
 function generateAR(num::Int, order::Int; nvar=1, stat=true, coefs=nothing)
     if isnothing(coefs) && stat
         coefs = generate_coefficients(order)
@@ -77,7 +101,7 @@ function generateAR(num::Int, order::Int; nvar=1, stat=true, coefs=nothing)
     data = Vector{Vector{Float64}}(undef, num+3*order)
     data[1] = inits
     for i in 2:num+3*order
-        data[i] = insert!(data[i-1][1:end-1], 1, rand(Distributions.Normal(coefs'data[i-1], sqrt(nvar)), 1)[1])
+        data[i] = insert!(data[i-1][1:end-1], 1, rand(Distributions.Normal(coefs'data[i-1], sqrt(nvar[])), 1)[1])
     end
     data = data[1+3*order:end]
     return coefs, data
