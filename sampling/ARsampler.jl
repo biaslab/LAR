4e1204995b7844f2f0b1c92d33e60612de5ec9c7
albertpod <albert.podusenko@gmail.com>
2020-01-28 14:36:49 +0100

    Fix sampling

--- sampling/ARsampler.jl
+++ sampling/ARsampler.jl
@@ -21,7 +21,7 @@ diagAR(dim) = Matrix{Float64}(I, dim, dim)
 end
 
 
-@model HAR(y, order=1, onoise=1.0) = begin
+@model HAR(y, order=1, onoise=2.0) = begin
     x = Vector(undef, length(y))
     θ1 = Vector(undef, length(y))
     v_θ1 ~ InverseGamma(3, 2)
@@ -29,7 +29,7 @@ end
     θ2 ~ MvNormal(zeros(order), diagAR(order))
     for i in 1:order
         x[i] ~ Normal(0, 1)
-        θ1[i] ~ Normal(0, 1)
+        θ1[i] ~ Normal(1, 1)
     end
     for i in order+1:length(y)
         θ1[i] ~ Normal(θ2'reverse(θ1[i-order:i-1]), sqrt(v_θ1))
@@ -55,31 +55,37 @@ x = [x[1] for x in dataHAR[3]]
 v_y = 2.0
 y = [xi[1] + sqrt(v_y)*randn() for xi in dataHAR[3]];
 
-m_har = HAR(y, 2, 2.0)
+m_har = HAR(y, 1, 2.0)
 
-chain = sample(m_har, SMC(), 1000)
-
-m_st = [mean(chain[Symbol("x[$i]")])[2][1] for i in 1:length(x)]
-m_θ = [mean(chain[Symbol("θ1[$i]")])[2][1] for i in 1:length(x)]
+chain = sample(m_har, SMC(), resampler_threshold=.3, 1000)
+m_st = [mean(chain[Symbol("x[$i]")].value) for i in 1:length(x)]
+v_st = [std(chain[Symbol("x[$i]")].value) for i in 1:length(x)]
+m_θ = [mean(chain[Symbol("θ1[$i]")].value) for i in 1:length(θ)]
+v_θ = [std(chain[Symbol("θ1[$i]")].value) for i in 1:length(θ)]
 coef = mean(chain[:θ2])[2]
-v_x = mean(chain[:v_x])[2][1]
-v_θ1 = mean(chain[:v_θ1])[2]
+#v_x = mean(chain[:v_x])[2][1]
+#v_θ1 = mean(chain[:v_θ1])[2]
 
-from = 800
-upto = 900
+logPDF(mx, x, vx) = -0.5*log(2*pi*vx) - ((mx - x)^2)/(2 * vx)
+logPDFsHAR = [logPDF(x[t], m_st[t], v_st[t]) for t in 1:length(y)]
+sum(logPDFsHAR)
+from = 630
+upto = 1000
 scatter(y, ylims=(minimum(y[from:upto]), maximum(y[from:upto])), markershape = :xcross, markeralpha = 0.6,
         markersize = 2, xlabel="time t", ylabel="value", label="observations", xlims=(from, upto))
 plot!(x, color=:magenta, label=L"real \quad x_t", title="AR($ARorder) process")
-plot!(m_st, ribbon=sqrt.(v_x),
+plot!(m_st, ribbon=sqrt.(v_st),
       linestyle=:dash, linewidth = 2,
       color=:black,
       fillalpha = 0.2,
       fillcolor = :black,
       label="inferred")
 
+
+
 plot(xlims=(from, upto), )
 plot!(θ[2:end], color=:red, label=L"true \quad \theta^{(1)}", title="Generated AR($ARorder) process")
-plot!(m_θ, ribbon=sqrt.(v_θ1),
+plot!(m_θ, ribbon=sqrt.(v_θ),
       linestyle=:dash, linewidth = 2,
       color=:black,
       fillalpha = 0.2,
